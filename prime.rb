faf8768db7959c3d64ede3bed5eb43872ce23081
Ignacio Del Busto <gidelbusto@gmail.com>
2020-06-10 03:13:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,6 @@
 def prime(n) # starting with 2 because testing for a prime means you don't want to test division by 1
   2.upto(Math.sqrt(n)) do |x| # testing up to the square root of the number because going past there is excessive
     if n % x == 0
-      # n is the number being called from the program
-      # x is the number we're dividing by, counting from 2 up to the square root of the number
       return false # this means the number is not prime
     else
       return true # this means the number is prime
