1544302f21f534a8021b9df32f9c97c84ab2abb8
Ignacio Del Busto <gidelbusto@gmail.com>
2020-06-10 02:59:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,8 @@
 # Add  code here!
 
- def is_prime(integer)
-  for integer in 2..(integer - 1)
-   if (integer % d) == 0
+def is_prime(n)
+  for d in 2..(n - 1)
+   if (n % d) == 0
     return false
    end
   end
-
-  true
- end
