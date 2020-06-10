275a98e24d8001c9de10629de5696a061421f913
Ignacio Del Busto <gidelbusto@gmail.com>
2020-06-10 03:04:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,6 @@
 # Add  code here!
 
-class DetermineIfPrime
-def initialize (nth_value)
-@nth_value = nth_value
-primetest
-end
-
- def primetest
- for test_value in [2, 3, 5, 7, 9, 11, 13] do
-  if (@nth_value % test_value) == 0
-   puts ("#{@nth_value} is not divisible by #{test_value}")
-  else
-   puts ("This is not a prime number since this is divisible by #{test_value}")
-  break
-  end
- end
- end
+def is_prime?(num)
+   Math.sqrt(num).floor.downto(2).each {|i| return false if num % i == 0}
+   true
 end
