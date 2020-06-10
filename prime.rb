479ad1b941da020ff60801a5d5428240d142694c
Ignacio Del Busto <gidelbusto@gmail.com>
2020-06-10 03:10:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
 # Add  code here!
 
 def prime?(num)
-  (2..Math.sqrt(num)).each { |i|
-  if num % i == 0 && i < num
+    2.upto(Math.sqrt(num).ceil) do |i|
+        break if num%i==0
+        return true if i==Math.sqrt(num).ceil   
+    end
     return false
-  end
-  }
-  true
-  end
+end
