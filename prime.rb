035818a5c5ebffdd50904bb0ca81da0d7d45d16a
Ignacio Del Busto <gidelbusto@gmail.com>
2020-06-10 03:14:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,9 @@
 def prime?(n)
   2.upto(Math.sqrt(n)) do |x| 
     if n % x == 0
-      return false # this means the number is not prime
+      return false
     else
-      return true # this means the number is prime
+      return true
     end 
   end
 end
